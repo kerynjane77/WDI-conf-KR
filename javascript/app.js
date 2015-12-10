a3ded3135e84176501e11e314cebf1a5a5d16c2d
Keryn R <kerynjane77@gmail.com>
2015-12-10 16:52:04 +1100

    Keryn WDIconf

---
+++ javascript/app.js
@@ -0,0 +1,29 @@
+// function to open and close modal with button and X
+
+$("#open-modal").on('click', function() {
+  $("input").val("");  // clear text from input form
+  $("#modal").toggle();
+});
+
+
+$("#close-modal").on("click", function(){
+  $("#modal").toggle();
+});
+
+
+// function to make smooth scrolling between sections
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+        scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
